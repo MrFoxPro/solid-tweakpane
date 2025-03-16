7c6ba1aea3194408fffdb34ad31ab772e5447447
mrfoxpro <tuningiposadka@gmail.com>
2025-03-17 00:49:04 +0500

    minor fixes

--- src/controls.tsx
+++ src/controls.tsx
@@ -1,84 +1,84 @@
-import { createEffect, on, onCleanup, JSX, VoidProps } from 'solid-js'
-import { Tweakpane, TWPBaseProps, TWPBindGroup, useTWPBingGroup, useTWPRoot } from './base'
-import type { Bindable } from '@tweakpane/core'
-import type { InputBindingApi, MonitorBindingApi, InputParams, MonitorParams } from 'tweakpane'
-import type { BladeRackApi } from '@tweakpane/core/dist/cjs/blade/common/api/blade-rack'
+import { createEffect, on, onCleanup, JSX, VoidProps } from "solid-js"
+import { Tweakpane, TWPBaseProps, TWPBindGroup, useTWPBingGroup, useTWPRoot } from "./base"
+import type { Bindable } from "@tweakpane/core"
+import type { InputBindingApi, MonitorBindingApi, InputParams, MonitorParams } from "tweakpane"
+import type { BladeRackApi } from "@tweakpane/core/dist/cjs/blade/common/api/blade-rack"
 
 type AvailableAPI = InputBindingApi<unknown, any> | MonitorBindingApi<any>
 type TWPControlBaseProps = VoidProps &
-   TWPBaseProps<AvailableAPI> & {
-      target?: Bindable
-      key?: string
-      initialValue?: any
-   }
+	TWPBaseProps<AvailableAPI> & {
+		target?: Bindable
+		key?: string
+		initialValue?: any
+	}
 
 type TWPControlFactory<TProps> = (
-   root: BladeRackApi,
-   binding: Bindable,
-   props: TProps
+	root: BladeRackApi,
+	binding: Bindable,
+	props: TProps
 ) => [AvailableAPI, JSX.Element?]
 function createTWPControl<TInitProps, TProps extends TWPControlBaseProps = TInitProps & TWPControlBaseProps>(
-   initFn: TWPControlFactory<TProps>
+	initFn: TWPControlFactory<TProps>
 ) {
-   return function (props: TProps) {
-      const root = useTWPRoot()
-      if (!root) throw new Error(`Use tweakpane controls within <${nameof(Tweakpane)}>`)
-      let binding = props.target ?? useTWPBingGroup()
-      if (!binding && props.key) {
-         throw new Error(
-            `In case of usage <TWPControl> with key, place it within <${nameof(
-               TWPBindGroup
-            )}> and provide taget`
-         )
-      }
-      if (binding && !Object.hasOwn(binding, props.key)) {
-         throw new Error(`There is no key ${props.key} in binding ${binding}`)
-      }
-      if (!binding && !props.key) {
-         if (!props.initialValue) {
-            throw new Error(
-               `In case of usage <TWPControl> without target and key, provide ${nameof(
-                  props.initialValue
-               )} prop`
-            )
-         }
-         binding = { value: props.initialValue }
-         props.key = 'value'
-      }
-      const [comp, element] = initFn(root, binding, props)
-      createEffect(
-         on(
-            () => binding[props.key],
-            () => comp.refresh(),
-            { defer: true }
-         )
-      )
-      onCleanup(() => {
-         if (!comp) return
-         root.remove(comp)
-         comp.dispose()
-      })
-      props.ref?.(comp)
-      if (element) return element
-   }
+	return function (props: TProps) {
+		const root = useTWPRoot()
+		if (!root) throw new Error(`Use tweakpane controls within <${nameof(Tweakpane)}>`)
+		let binding = props.target ?? useTWPBingGroup()
+		if (!binding && props.key) {
+			throw new Error(
+				`In case of usage <TWPControl> with key, place it within <${nameof(
+					TWPBindGroup
+				)}> and provide taget`
+			)
+		}
+		if (binding && !Object.hasOwn(binding, props.key)) {
+			throw new Error(`There is no key ${props.key} in binding ${binding}`)
+		}
+		if (!binding && !props.key) {
+			if (!props.initialValue) {
+				throw new Error(
+					`In case of usage <TWPControl> without target and key, provide ${nameof(
+						props.initialValue
+					)} prop`
+				)
+			}
+			binding = { value: props.initialValue }
+			props.key = "value"
+		}
+		const [comp, element] = initFn(root, binding, props)
+		createEffect(
+			on(
+				() => binding[props.key],
+				() => comp.refresh(),
+				{ defer: true }
+			)
+		)
+		onCleanup(() => {
+			if (!comp) return
+			root.remove(comp)
+			comp.dispose()
+		})
+		props.ref?.(comp)
+		if (element) return element
+	}
 }
 
 type TWPInputInputProps = {
-   params?: InputParams
-   onChange?: Parameters<InputBindingApi<unknown, string>['on']>[1]
+	params?: InputParams
+	onChange?: Parameters<InputBindingApi<unknown, string>["on"]>[1]
 }
 export const TWPInput = createTWPControl<TWPInputInputProps>((root, binding, props) => {
-   const input = root.addInput(binding, props.key, props.params)
-   props.onChange && input.on('change', props.onChange)
-   return [input]
+	const input = root.addInput(binding, props.key, props.params)
+	props.onChange && input.on("change", props.onChange)
+	return [input]
 })
 
 type TWPMonitorProps = {
-   params?: MonitorParams
-   onUpdate?: Parameters<MonitorBindingApi<unknown>['on']>[1]
+	params?: MonitorParams
+	onUpdate?: Parameters<MonitorBindingApi<unknown>["on"]>[1]
 }
 export const TWPMonitor = createTWPControl<TWPMonitorProps>((root, binding, props) => {
-   const monitor = root.addMonitor(binding, props.key, props.params)
-   props.onUpdate && monitor.on('update', props.onUpdate)
-   return [monitor]
+	const monitor = root.addMonitor(binding, props.key, props.params)
+	props.onUpdate && monitor.on("update", props.onUpdate)
+	return [monitor]
 })
--- src/ui.tsx
+++ src/ui.tsx
@@ -1,80 +1,80 @@
 import {
-  ParentProps,
-  splitProps,
-  onCleanup,
-  VoidProps,
-  JSX,
-  FlowProps,
-  createEffect,
-  createContext,
-  useContext,
-  onMount,
-} from 'solid-js'
-import { Tweakpane, TWPBaseProps, TWPGroup, useTWPRoot } from './base'
-import type { FolderApi, FolderParams, TabParams, SeparatorParams } from 'tweakpane'
-import type { ButtonApi, SeparatorApi, TabApi, TabPageParams } from '@tweakpane/core'
-import type { BladeRackApi } from '@tweakpane/core/dist/cjs/blade/common/api/blade-rack'
+	ParentProps,
+	splitProps,
+	onCleanup,
+	VoidProps,
+	JSX,
+	FlowProps,
+	createEffect,
+	createContext,
+	useContext,
+	onMount,
+} from "solid-js"
+import { Tweakpane, TWPBaseProps, TWPGroup, useTWPRoot } from "./base"
+import type { FolderApi, FolderParams, TabParams, SeparatorParams } from "tweakpane"
+import type { ButtonApi, SeparatorApi, TabApi, TabPageParams } from "@tweakpane/core"
+import type { BladeRackApi } from "@tweakpane/core/dist/cjs/blade/common/api/blade-rack"
 
 type AvailableAPI = FolderApi | ButtonApi | TabApi | SeparatorApi
 type TWPUIBaseProps = TWPBaseProps<AvailableAPI>
 type TWPUIInitFn<TProps> = (root: BladeRackApi, props: TProps) => [AvailableAPI, JSX.Element?]
 
 function createTWPUI<TInitProps, TProps extends TWPUIBaseProps = TInitProps & TWPUIBaseProps>(
-  initFn: TWPUIInitFn<TProps>
+	initFn: TWPUIInitFn<TProps>
 ) {
-  return function (props: TProps) {
-    const root = useTWPRoot()
-    if (!root) throw new Error(`Use tweakpane controls within <${nameof(Tweakpane)}>`)
-    const [comp, element] = initFn(root, props)
-    onCleanup(() => {
-      if (!comp) return
-      comp.dispose()
-      root.remove(comp)
-    })
-    props.ref?.(comp)
-    if (element) return element
-  }
+	return function(props: TProps) {
+		const root = useTWPRoot()
+		if (!root) throw new Error(`Use tweakpane controls within <${nameof(Tweakpane)}>`)
+		const [comp, element] = initFn(root, props)
+		onCleanup(() => {
+			if (!comp) return
+			comp.dispose()
+			root.remove(comp)
+		})
+		props.ref?.(comp)
+		if (element) return element
+	}
 }
 export const TWPFolder = createTWPUI<ParentProps<FolderParams>>((root, props) => {
-  const [compProps, folderProps] = splitProps(props, ['children'])
-  const folder = root.addFolder(folderProps)
-  return [folder, <TWPGroup root={folder}>{compProps.children}</TWPGroup>]
+	const [compProps, folderProps] = splitProps(props, ["children"])
+	const folder = root.addFolder(folderProps)
+	return [folder, <TWPGroup root={folder}>{compProps.children}</TWPGroup>]
 })
 
 type TWPButtonProps = {
-  onClick?: Parameters<ButtonApi['on']>[1]
+	onClick?: Parameters<ButtonApi["on"]>[1]
 } & FolderParams
 export const TWPButton = createTWPUI<TWPButtonProps>((root, props) => {
-  const button = root.addButton(props)
-  props.onClick && button.on('click', props.onClick)
-  createEffect(() => {
-    button.title = props.title
-  })
-  return [button]
+	const button = root.addButton(props)
+	props.onClick && button.on("click", props.onClick)
+	createEffect(() => {
+		button.title = props.title
+	})
+	return [button]
 })
 
 export const TWPSeparator = createTWPUI<VoidProps<SeparatorParams>>((root, props) => {
-  const separator = root.addSeparator(props)
-  return [separator]
+	const separator = root.addSeparator(props)
+	return [separator]
 })
 
 const TabContext = createContext<TabApi>()
 const useTab = () => useContext(TabContext)
-export const TWPTab = createTWPUI<FlowProps<Omit<TabParams, 'pages'>>>((root, props) => {
-  const [compProps, tabProps] = splitProps(props, ['children'])
-  const tab = root.addTab({ ...tabProps, pages: [{ title: 'mock' }] })
-  onMount(() => tab.removePage(0))
-  return [tab, <TabContext.Provider value={tab}>{compProps.children}</TabContext.Provider>]
+export const TWPTab = createTWPUI<FlowProps<Omit<TabParams, "pages">>>((root, props) => {
+	const [compProps, tabProps] = splitProps(props, ["children"])
+	const tab = root.addTab({ ...tabProps, pages: [{ title: "mock" }] })
+	onMount(() => tab.removePage(0))
+	return [tab, <TabContext.Provider value={tab}>{compProps.children}</TabContext.Provider>]
 })
 
 export function TWPTabPage(props: ParentProps<TabPageParams>) {
-  const root = useTab()
-  if (!root) throw new Error(`Parent for <${nameof(TWPTabPage)}> should be <${nameof(TWPTab)}>`)
-  const [compProps, pageProps] = splitProps(props, ['children'])
-  const page = root.addPage(pageProps)
-  createEffect(() => (page.title = props.title))
-  onCleanup(() => {
-    root.removePage(root.pages.indexOf(page))
-  })
-  return <TWPGroup root={page}>{compProps.children}</TWPGroup>
+	const root = useTab()
+	if (!root) throw new Error(`Parent for <${nameof(TWPTabPage)}> should be <${nameof(TWPTab)}>`)
+	const [compProps, pageProps] = splitProps(props, ["children"])
+	const page = root.addPage(pageProps)
+	createEffect(() => (page.title = props.title))
+	onCleanup(() => {
+		root.removePage(root.pages.indexOf(page))
+	})
+	return <TWPGroup root={page}>{compProps.children}</TWPGroup>
 }
